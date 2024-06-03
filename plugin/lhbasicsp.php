fdb0902c3887145d2d2e6ef16e08ae377e8404e5
Luehrsen <gh-actions-Luehrsen@github.com>
2024-06-03 13:25:31 +0000

    Tagging 0.1.3

--- plugin/lhbasicsp.php
+++ plugin/lhbasicsp.php
@@ -9,7 +9,7 @@
  * Description: A plugin that provides basic functionality for our WordPress projects.
  * Author: Luehrsen // Heinrich
  * Author URI: https://www.luehrsen-heinrich.de
- * Version: 0.1.2
+ * Version: 0.1.3
  * Text Domain: lhbasicsp
  * Domain Path: /languages
  */
