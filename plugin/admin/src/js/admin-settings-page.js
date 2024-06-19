15147d74b5b521adac85b532281d06a48a5e13ff
Hendrik Luehrsen <hendrik@luehrsen.de>
2024-06-19 16:56:35 +0200

    Move the exposed objects into its own file

--- plugin/admin/src/js/admin-settings-page.js
+++ plugin/admin/src/js/admin-settings-page.js
@@ -1,11 +1,6 @@
 import { createRoot } from '@wordpress/element';
 import SettingsPage from './modules/settings-page';
-import SettingsObject from './modules/settings-object';
-
-/**
- * Expose the settings object to the global scope.
- */
-window.lhSettings = SettingsObject;
+import './globalVariables';
 
 document.addEventListener('DOMContentLoaded', () => {
 	const settingsPage = document.getElementById('admin-settings-page');
