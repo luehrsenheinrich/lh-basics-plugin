b15704b1cff9ea88253d784b13278ff9e3e5165d
Christopher Kanitz <ckanitz@users.noreply.github.com>
2025-02-10 14:46:14 +0100

    expose (LH)Icon component to lhbasics.components

--- plugin/admin/src/js/blocks-helper.js
+++ plugin/admin/src/js/blocks-helper.js
@@ -7,8 +7,10 @@
 
 // Import the IconSelectControl component
 import IconSelectControl from './components/icon-select-control';
+import Icon from './components/icon';
 
 // Register the IconSelectControl component to the global lhbasics.components namespace
 window.lhbasics = window.lhbasics || {};
 window.lhbasics.components = window.lhbasics.components || {};
 window.lhbasics.components.IconSelectControl = IconSelectControl;
+window.lhbasics.components.Icon = Icon;
