527e90b3d6029510a6e0c6167cc397bf06a9f4b4
Christopher Kanitz <ckanitz@users.noreply.github.com>
2025-02-28 15:32:03 +0100

    expose the components via lhbasics.components

--- plugin/admin/src/js/blocks-helper.js
+++ plugin/admin/src/js/blocks-helper.js
@@ -8,12 +8,19 @@
 // Import components
 import IconSelectControl from './components/icon-select-control';
 import LHIcon from './components/icon';
+import EntitySelectControl, {
+	PostSelectControl,
+	TaxonomySelectControl,
+} from './components/entity-select-control';
 
 window.lhbasics = window.lhbasics || {};
 window.lhbasics.components = window.lhbasics.components || {};
 
 // Register the IconSelectControl component to the global lhbasics.components namespace
 window.lhbasics.components.IconSelectControl = IconSelectControl;
+window.lhbasics.components.EntitySelectControl = EntitySelectControl;
+window.lhbasics.components.PostSelectControl = PostSelectControl;
+window.lhbasics.components.TaxonomySelectControl = TaxonomySelectControl;
 
 // Register the LHIcon component to the global lhbasics.components namespace
 window.lhbasics.components.LHIcon = LHIcon;
