d8a6cdfddd1321355eb068c0950c81d5c44d3ed0
Christopher Kanitz <ckanitz@users.noreply.github.com>
2026-07-02 14:11:31 +0200

    chore(main): release lh-basics-plugin-project 0.6.1

--- plugin/lhbasicsp.php
+++ plugin/lhbasicsp.php
@@ -10,7 +10,7 @@
  * Author: Luehrsen // Heinrich
  * Author URI: https://www.luehrsen-heinrich.de
  * x-release-please-start-version
- * Version: 0.6.0
+ * Version: 0.6.1
  * x-release-please-end
  * Text Domain: lhbasicsp
  * Domain Path: /languages
