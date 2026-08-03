e8f843dbe6a0299b7cc55092ddc22739b10c509b
Hendrik Luehrsen <Luehrsen@users.noreply.github.com>
2026-08-03 13:02:48 +0200

    feat: add application passwords module (#508)

--- plugin/inc/Plugin.php
+++ plugin/inc/Plugin.php
@@ -19,22 +19,24 @@ class Plugin {
 	/**
 	 * Constructor.
 	 *
-	 * @param Blocks\Blocks                     $blocks           The blocks component.
-	 * @param i18n\I18N                         $i18n             The i18n component.
-	 * @param Disable_Comments\Disable_Comments $disable_comments The disable comments component.
-	 * @param Gravity_Forms\Gravity_Forms       $gravity_forms    The gravity forms component.
-	 * @param Lazysizes\Lazysizes               $lazysizes        The lazysizes component.
-	 * @param Lightbox\Lightbox                 $lightbox         The lightbox component.
-	 * @param Settings\Settings                 $settings         The settings component.
-	 * @param Admin_UX\Admin_UX                 $admin_ux         The admin UX component.
-	 * @param Performance\Performance           $performance      The performance component.
-	 * @param Styles\Styles                     $styles           The styles component.
-	 * @param SVG\SVG                           $svg              The svg component.
-	 * @param Logging\Logs                      $logs             The logs component.
+	 * @param Blocks\Blocks                               $blocks           The blocks component.
+	 * @param i18n\I18N                                   $i18n             The i18n component.
+	 * @param Application_Passwords\Application_Passwords $application_passwords The application passwords component.
+	 * @param Disable_Comments\Disable_Comments           $disable_comments The disable comments component.
+	 * @param Gravity_Forms\Gravity_Forms                 $gravity_forms    The gravity forms component.
+	 * @param Lazysizes\Lazysizes                         $lazysizes        The lazysizes component.
+	 * @param Lightbox\Lightbox                           $lightbox         The lightbox component.
+	 * @param Settings\Settings                           $settings         The settings component.
+	 * @param Admin_UX\Admin_UX                           $admin_ux         The admin UX component.
+	 * @param Performance\Performance                     $performance      The performance component.
+	 * @param Styles\Styles                               $styles           The styles component.
+	 * @param SVG\SVG                                     $svg              The svg component.
+	 * @param Logging\Logs                                $logs             The logs component.
 	 */
 	public function __construct(
 		private Blocks\Blocks $blocks,
 		private i18n\I18N $i18n,
+		private Application_Passwords\Application_Passwords $application_passwords,
 		private Disable_Comments\Disable_Comments $disable_comments,
 		private Gravity_Forms\Gravity_Forms $gravity_forms,
 		private Lazysizes\Lazysizes $lazysizes,
